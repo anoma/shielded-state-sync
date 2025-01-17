53904e0d1b75342743e365880cc1ca987155e931
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-01-17 15:53:09 +0000

    Fix clippy

--- fuzzy-message-detection/src/fmd2.rs
+++ fuzzy-message-detection/src/fmd2.rs
@@ -385,8 +385,7 @@ mod tests {
         let mut csprng = rand_core::OsRng;
 
         let rates = RestrictedRateSet::new(5);
-        let (pk, sk) = <Fmd2 as FmdScheme>::generate_keys(&rates, &mut csprng);
-        let flag_cipher = <Fmd2 as FmdScheme>::flag(&pk, &mut csprng);
+        let (_pk, sk) = <Fmd2 as FmdScheme>::generate_keys(&rates, &mut csprng);
 
         assert!(<Fmd2 as FmdScheme>::extract(&sk, &[0, 0, 1]).is_none());
         assert!(<Fmd2 as FmdScheme>::extract(&sk, &[0, 1, 2, 3, 4, 5, 6]).is_none());
